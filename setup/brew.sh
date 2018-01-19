ffb7f838d4eb6b207e4f32c4709295f0ef609a6e
yangh <yanghai1306@gmail.com>
2018-01-19 18:27:11 +0800

    add brew

---
+++ setup/brew.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+source functions.sh
+
+#install Homebrew and create SSH key pairs
+
+if ! command -v brew >/dev/null; then
+    yyq_echo "Installing Homebrew ..."
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    yyq_echo "Homebrew installed"
+fi
+
+yyq_echo "Setting up Github SSH key pairs."
+echo "Please enter your github email."
+read github_email
+ssh-keygen -t rsa -b 4096 -C $github_email
+
+yyq_echo "Copying SSH key to your clipboard."
+pbcopy < ~/.ssh/id_rsa.pub
+
+yyq_echo "Add key to github to finish setup."
