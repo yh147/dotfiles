3d658853a39f071a06ed4f3ad8f7b05522165a1e
yangh <yanghai1306@gmail.com>
2018-01-22 17:56:06 +0800

    add vimrc script

---
+++ setup/vimrc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# install Vundle
+
+vundlePath="$HOME/.vim/bundle/Vundle.vim"
+if [ ! -d "$vundlePath" ]; then
+	git clone https://github.com/VundleVim/Vundle.vim.git "$vundlePath"
+fi
+
+cat ../vim/vimrc > $HOME/.vimrc
+vim +PluginInstall +qall
