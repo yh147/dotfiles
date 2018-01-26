1f42ebeee6c75e5d31ebb947669833c042b8f049
yangh <yanghai1306@gmail.com>
2018-01-26 23:50:00 +0800

    java development

--- setup/install.sh
+++ setup/install.sh
@@ -7,3 +7,4 @@ source utilities.sh
 source vimrc.sh
 source bash_profile.sh
 source web.sh
+source java.sh
---
+++ setup/java.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+# java development
+
+brew cask install java
+brew cask install intellij-idea
