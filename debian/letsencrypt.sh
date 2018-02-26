2dbc1d3dc4e36188d9eae6877364e3c385c88759
yangh <512747471@qq.com>
2018-02-26 22:56:22 +0800

    update https

--- debian/letsencrypt.sh
+++ debian/letsencrypt.sh
@@ -2,5 +2,5 @@
 
 cd /home
 git clone https://github.com/letsencrypt/letsencrypt
-cd letsencrype
+cd /home/letsencrypt
 ./letsencrypt-auto certonly --standalone --email yanghai1306@gmail.com -d yangyaqi.cn -d www.yangyaqi.cn -d wx.yangyaqi.cn -d mirrors.yangyaqi.cn
