9503f111414d3ae1312c0b010054e78edb649578
yangh <yanghai1306@gmail.com>
2018-03-17 21:46:51 +0800

    mysql install

--- debian/mysql.sh
+++ debian/mysql.sh
@@ -4,3 +4,9 @@ function grant() {
   grant all privileges on *.* to 'root'@'%'identified by 'passwd' with grant option;
   ALL PRIVILEGES;
 }
+
+function install() {
+	yum -y install mariadb mariadb-server
+	systemctl start mariadb
+	systemctl enable mariadb
+}
