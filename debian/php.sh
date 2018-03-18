a7618e264aa6d51df76ee3819c0d8da338b4ba02
yangh <yanghai1306@gmail.com>
2018-03-18 21:45:27 +0800

    php.sh add centos7 support

--- debian/php.sh
+++ debian/php.sh
@@ -11,14 +11,24 @@ function install_php() {
 }
 
 function check_dependence() {
-	apt-get install -y gcc
-	apt-get install -y make
-	apt-get install -y libxml2-dev
-	apt-get install -y openssl libssl-dev
-	apt-get install -y libpng-dev
-	apt-get install -y libfreetype6-dev
-	apt-get install -y libmcrypt-dev
-	ln -s /usr/lib/x86_64-linux-gnu/libssl.so  /usr/lib
+	version=`cat /proc/version`
+	if [[ $version =~ "centos" ]]; then
+		yum install -y libxml2
+		yum install -y libxml2-devel
+		yum install -y curl-devel
+		yum install -y libpng
+		yum install -y libpng-devel
+		yum install -y freetype-devel
+	elif [[ $version =~ "debian" ]]; then
+		apt-get install -y gcc
+		apt-get install -y make
+		apt-get install -y libxml2-dev
+		apt-get install -y openssl libssl-dev
+		apt-get install -y libpng-dev
+		apt-get install -y libfreetype6-dev
+		apt-get install -y libmcrypt-dev
+		ln -s /usr/lib/x86_64-linux-gnu/libssl.so  /usr/lib
+	fi
 }
 
 function add_user() {
@@ -37,7 +47,7 @@ function config_php() {
 	./configure  --prefix=$php_install_dir \
 	--with-gd \
 	--with-zlib \
-	--with-curl=/usr/local/curl \
+	--with-curl \
 	--with-freetype-dir \
 	--enable-gd-native-ttf \
 	--enable-mysqlnd \
@@ -51,10 +61,8 @@ function config_php() {
 	--with-fpm-group=php &&\
 	make && make install
 	cp php.ini-production $php_install_dir/lib/php.ini
-	cp sapi/fpm/init.d.php-fpm /etc/init.d/php-fpm
 	cp $php_install_dir/etc/php-fpm.conf.default $php_install_dir/etc/php-fpm.conf
 	cp $php_install_dir/etc/php-fpm.d/www.conf.default $php_install_dir/etc/php-fpm.d/www.conf
-	chmod +x /etc/init.d/php-fpm
 }
 
 function clear_src() {
@@ -63,23 +71,30 @@ function clear_src() {
 }
 
 function start_php() {
-	/etc/init.d/php-fpm start
+	systemctl start php-fpm
 }
 
 function stop_php() {
-	/etc/init.d/php-fpm stop
+	systemctl stop php-fpm
 }
 
 function uninstall_php() {
 	stop_php
-	rm -rf /etc/init.d/php-fpm
 	rm -rf /home/php7
+}
+
+function init_php() {
+	wget -P /etc/systemd/system/ "https://raw.githubusercontent.com/yh1306/dotfiles/master/service/php-fpm.service"
+	systemctl start php-fpm
+	systemctl enable php-fpm
+	systemctl status php-fpm
 }
 
 case $1 in
 	install)
 		echo "start install php"
 		install_php
+		init_php
 		;;
 	uninstall)
 		echo "uninstall php"
@@ -93,19 +108,7 @@ case $1 in
 		echo "stop php"
 		stop_php
 		;;
-	reload)
-		echo "reload php"
-		reload_php
-		;;
-	check)
-		echo "check php conf"
-		check_php_conf
-		;;
-	info)
-		echo "php configure info"
-		config_info
-		;;
 	*)
-		echo $"Usage: $0 {start|stop|reload|check|install|uninstall|info}"
+		echo $"Usage: $0 {start|stop|install|uninstall}"
 		;;
 esac
