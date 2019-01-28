cf128f1397f7f95a54bfa90497fc9911618b9a1f
yangh <yanghai1306@gmail.com>
2019-01-28 23:48:11 +0800

    Update java.sh

--- setup/java.sh
+++ setup/java.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 # java development
 
-brew cask install java
-brew cask install intellij-idea
+#brew cask install java
+#brew cask install intellij-idea
 brew install maven
 brew install mysql
