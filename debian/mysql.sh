b9c5b0535405285fc6de945dc53e8e7cf5375c84
yangh <yanghai1306@gmail.com>
2019-03-24 22:52:04 +0800

    Update mysql.sh

--- debian/mysql.sh
+++ debian/mysql.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-function grant() {
-  grant all privileges on *.* to 'root'@'%'identified by 'passwd' with grant option;
-  FLUSH PRIVILEGES;
-}
-
 function install() {
 	wget https://dev.mysql.com/get/mysql-apt-config_0.8.6-1_all.deb
 	dpkg -i mysql-apt-config_0.8.6-1_all.deb
 	apt-get update
 	apt-get install mysql-server
 }
+
+first=$1
+case $first in
+	install)
+		echo "start install mysql"
+		install
+		;;
+	*)
+		echo $"Usage: $ {install8|uninstall}"
+		;;
+esac
